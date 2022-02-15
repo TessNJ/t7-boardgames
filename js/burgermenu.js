7437def172ce3d0ba90b7b8aacd258ff53e97fe1
Greta Rimkute <gretarimkuter@gmail.com>
2022-02-15 22:25:55 +0100

    added active istead of hover for narrow n wide vie

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -1,14 +1,26 @@
 const burger = document.querySelector(".burgerMenu");
 const navMenu = document.querySelector(".nav-menu");
+const subNavCat = document.querySelector("#category-nav-link");
+const list = document.querySelector(".navbar ul li");
+
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    list.classList.remove("active");
 
 } )
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click"), ()=>{
-    burger.classList.remove("active"); 
-    navMenu.classList.remove("active"); 
-    })
+
+subNavCat.addEventListener("click", () => {
+    list.classList.toggle("active");
+} )
+
+
+document.querySelectorAll(".sub-nav li").forEach(n => n.
+    addEventListener("click", () =>{
+        burger.classList.remove("active"); 
+        navMenu.classList.remove("active"); 
+        list.classList.remove("active");
+    }
+))
